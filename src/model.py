85eeaf636da97915396b586528e3f89e1348f171
Christopher-preston03 <prestonchris03@gmail.com>
2025-12-29 21:44:21 -0600

    Add batch normalization and effective dropout to CNN

--- src/model.py
+++ src/model.py
@@ -4,23 +4,27 @@ from keras import layers, models
 def build_model(num_classes):
     inputs = layers.Input(shape=(32, 32, 3))
 
-    x = layers.Conv2D(32, (3, 3), activation='relu')(inputs)
+    x = layers.Conv2D(32, 3, padding='same')(inputs)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
     x = layers.MaxPooling2D()(x)
-    x = layers.Conv2D(64, (3, 3), activation='relu')(x)
+
+    x = layers.Conv2D(64, 3, padding='same')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation("relu")(x)
     x = layers.MaxPooling2D()(x)
 
     x = layers.Flatten()(x)
     embedding = layers.Dense(64, activation='relu', name='embedding')(x)
-    outputs = layers.Dense(num_classes, activation='softmax')(embedding)
+    x = layers.Dropout(0.4)(embedding)
+    outputs = layers.Dense(num_classes, activation='softmax')(x)
 
-    model = models.Model(inputs=inputs, outputs=outputs)
+    model = models.Model(inputs, outputs)
 
     model.compile(
         optimizer='adam',
         loss='sparse_categorical_crossentropy',
         metrics=["accuracy"]
     )
-
-    model.summary()
 
     return model
